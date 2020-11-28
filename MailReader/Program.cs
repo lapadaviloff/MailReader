f9121112eeebaf8430db68a0ccc10bc10f7bbf4a
lapadaviloff <lapadaviloff@yandex.ru>
2020-11-28 18:06:13 +0300

    start

---
+++ MailReader/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * прогамма для чтения сообщений с почтовых ящиков 
+ * 28.11.2020
+ * miam.devsoft@gmail.com(lapadaviloff@yandex.ru)
+ */
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailReader
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
